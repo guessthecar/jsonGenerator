d747e5f7c604a1c1495c9df2cdc2f5003b58c7dd
Enrico Marcantoni <e.marcantoni@archimede.nu>
2023-04-03 00:17:07 +0200

    fatto tu

--- CarQuizJsonTool/src/app/app.component.ts
+++ CarQuizJsonTool/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { CarJson } from 'src/model/car_list'  
+import { CarJson } from 'src/model/car_list'
+import { Pipe, PipeTransform } from '@angular/core';
+
 
 
 interface JSONResult {
@@ -35,25 +37,30 @@ enum QuizType {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'proj';
 
   jsonResult: JSONResult = CarJson;
   carList: Car[] = this.jsonResult.result;
+  correctCar: Car | undefined;
+  optionsCar: Car[] = [];
 
   challengeLevel: number = 0;
   quizType: QuizType = QuizType.none;
+  query: any;
+
 
   onInputChange(event: Event) {
-    
+
     this.challengeLevel = (event.target as HTMLInputElement).valueAsNumber
-    
+
   }
 
   challengeSelected(): Boolean {
 
     return this.challengeLevel != 0;
 
-  } 
+  }
 
   quizTypeSelected(): Boolean {
 
@@ -61,4 +68,56 @@ export class AppComponent {
 
   }
 
+  isTheRight(car: Car) {
+    return car == this.correctCar;
+  }
+
+  isInTheOptions(car: Car) {
+    return this.optionsCar.indexOf(car) != -1;
+  }
+
+  markRight(event: any) {
+    var target = event.target || event.srcElement || event.currentTarget;
+    var car = target.parentElement.parentElement.parentElement;
+    var modello = car.dataset.modello;
+    var anno = car.dataset.anno
+
+    this.correctCar = this.carList.find(c => c.model == modello && c.year == anno);
+
+
+
+  }
+
+  markOption(event: any) {
+    var target = event.target || event.srcElement || event.currentTarget;
+    var car = target.parentElement.parentElement.parentElement;
+    var modello = car.dataset.modello;
+    var anno = car.dataset.anno
+
+    var carToAdd = this.carList.find(c => c.model == modello && c.year == anno);
+    //Controllare se già presente in array
+    if (carToAdd != null) {
+      if (this.optionsCar.length >= 3) {
+        this.optionsCar.shift();
+      }
+      this.optionsCar.push(carToAdd);
+    }
+
+  }
+
+}
+
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+  public transform(value: any, keys: string, term: string) {
+
+    if (!term) return value;
+    return (value || []).filter((item: any) => {
+      return keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key]));
+    });
+
+  }
 }
--- CarQuizJsonTool/src/app/app.module.ts
+++ CarQuizJsonTool/src/app/app.module.ts
@@ -4,20 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgOptimizedImage } from '@angular/common'
+import { FormsModule } from '@angular/forms';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
+import { SearchPipe } from './app.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    MatSelectModule
+    MatSelectModule,
+    NgOptimizedImage,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
