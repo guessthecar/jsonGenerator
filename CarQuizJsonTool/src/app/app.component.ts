0aa6acfe822e3f9da3e7f2190f90a2ff056607aa
Enrico Marcantoni <e.marcantoni@archimede.nu>
2023-04-03 23:08:40 +0200

    Aggiunto export

--- CarQuizJsonTool/src/app/app.component.ts
+++ CarQuizJsonTool/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CarJson } from 'src/model/car_list'
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 
 
@@ -31,10 +32,17 @@ enum QuizType {
 
 }
 
+interface Quiz {
+  difficoltà: number,
+  tipoImmagine: QuizType,
+  rispostaCorretta: Car | undefined,
+  opzioni: Car[]
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
 
@@ -44,10 +52,13 @@ export class AppComponent {
   carList: Car[] = this.jsonResult.result;
   correctCar: Car | undefined;
   optionsCar: Car[] = [];
+  quizList: Quiz[] = [];
 
   challengeLevel: number = 0;
   quizType: QuizType = QuizType.none;
   query: any;
+  downloadJsonHref: any;
+  constructor(private sanitizer: DomSanitizer) { }
 
 
   onInputChange(event: Event) {
@@ -63,9 +74,22 @@ export class AppComponent {
   }
 
   quizTypeSelected(): Boolean {
-
     return this.quizType != QuizType.none;
+  }
+  correctCarSelected(): Boolean {
+    return this.correctCar != null;
+  }
 
+  optionsCarNotEmpty(): Boolean {
+    return this.optionsCar.length > 0;
+  }
+
+  optionsCarReady(): Boolean {
+    return this.optionsCar.length == 3;
+  }
+
+  thereAreQuiz(): Boolean {
+    return this.quizList.length != 0;
   }
 
   isTheRight(car: Car) {
@@ -76,15 +100,27 @@ export class AppComponent {
     return this.optionsCar.indexOf(car) != -1;
   }
 
+  emptyAll() {
+    //TODO: Quado resetto il challange levelo si deve svuotare aggiornare lo slider in view
+    this.challengeLevel = 0;
+    this.quizType = QuizType.none;
+    this.correctCar = undefined;
+    this.optionsCar = [];
+  }
+
   markRight(event: any) {
     var target = event.target || event.srcElement || event.currentTarget;
     var car = target.parentElement.parentElement.parentElement;
     var modello = car.dataset.modello;
     var anno = car.dataset.anno
 
+
     this.correctCar = this.carList.find(c => c.model == modello && c.year == anno);
 
-
+    //Controllo se la macchina era tra le opzioni
+    if (this.optionsCar.length > 0 && this.correctCar != null && this.isInTheOptions(this.correctCar)) {
+      this.optionsCar.splice(this.optionsCar.indexOf(this.correctCar), 1);
+    }
 
   }
 
@@ -103,8 +139,26 @@ export class AppComponent {
       this.optionsCar.push(carToAdd);
     }
 
+    //Controllo se la macchina precedentemente era la macchina corretta
+    if (this.correctCar != null && this.correctCar == carToAdd) {
+      this.correctCar = undefined;
+    }
+
   }
 
+  generateDownloadJsonUri() {
+    var quiz: Quiz = {
+      difficoltà: this.challengeLevel,
+      tipoImmagine: this.quizType,
+      rispostaCorretta: this.correctCar,
+      opzioni: this.optionsCar
+    }
+    this.emptyAll();
+    this.quizList.push(quiz);
+    var theJSON = JSON.stringify(this.quizList);
+    var uri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8," + encodeURIComponent(theJSON));
+    this.downloadJsonHref = uri;
+  }
 }
 
 
